60e913662fda2dc2cdcf50f05202b405f3756b20
DNA <38492909+theblackdna@users.noreply.github.com>
2023-06-10 21:48:52 +0800

    Update streamlit_app-checkpoint.py

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 import openai
-openai.api_key = st.secrets["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"]
+openai.api_key = st.secrets["sk-QxfCbhkXZrTLJPHmSaVCT3BIbkFJImW8LGZ5HRezx49IxaEi"]
 import hashlib
 from PIL import Image
 
